8356fb90dd575c7e3f90a1fb70abb31f53edebe8
Nimish <shailbandha@gmail.com>
2021-08-11 13:55:34 +0530

    disable buttons

--- react-quiz/src/Components/App.js
+++ react-quiz/src/Components/App.js
@@ -6,10 +6,10 @@ const Ques = [
   {
     question: 'This is question 1',
     answer: 'This is answer 1',
-    option1: 'This is option 1',
-    option2: 'This is option 2',
-    option3: 'This is option 3',
-    option4: 'This is option 4',
+    option1: ['This is option 1', 1],
+    option2: ['This is option 2', 2],
+    option3: ['This is option 3', 3],
+    option4: ['This is option 4', 4],
     correct: 'option1',
   },
   {
@@ -56,6 +56,14 @@ function App() {
   const [input, setInput] = useState('');
   const [queNo, setQueNo] = useState(1);
   // console.log(userName)
+
+  const optionClick = (id) => { //* it will disable other options which are not selected
+    const ids = [1, 2, 3, 4]; //? reference for ids
+    document.getElementById(id).style.backgroundColor = "blue"; //? selected button
+    var res = ids.filter(item => item !== id) //! ids which are not clicked
+    var disable = res.map(item => document.getElementById(item).disabled = true); //? disable other options
+    var disableColor = res.map(item => document.getElementById(item).style.backgroundColor = "rgb(51, 51, 51)"); //? disabled color
+  }
   return (
     <div>
       <div id="home">
@@ -66,10 +74,14 @@ function App() {
             <div className="quesNo">Q{queNo}.{Ques[0].question}</div>
           </div>
           <div className="options">
-            <div className="option">{Ques[0].option1}</div>
-            <div className="option">{Ques[0].option2}</div>
-            <div className="option">{Ques[0].option3}</div>
-            <div className="option">{Ques[0].option4}</div>
+            <button onClick={() => optionClick(Ques[0].option1[1])}
+              className="option" id="1">{Ques[0].option1[0]}</button><br />
+            <button onClick={() => optionClick(Ques[0].option2[1])}
+              className="option" id="2">{Ques[0].option2[0]}</button><br />
+            <button onClick={() => optionClick(Ques[0].option3[1])}
+              className="option" id="3">{Ques[0].option3[0]}</button><br />
+            <button onClick={() => optionClick(Ques[0].option4[1])}
+              className="option" id="4">{Ques[0].option4[0]}</button>
           </div>
         </div> : null}
       </div>
